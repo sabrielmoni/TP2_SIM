28f9fd67d7c57a111d26857222df983f52af36f0
Sabrina Elmoni <sabrina.elmoni@gmail.com>
2023-04-17 16:04:22 -0300

    ui conectada

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 import sys
+from ui import *
 
 class AppWin(QMainWindow, Ui_MainWindow):
     def __init__(self):
 
         QMainWindow.__init__(self)
-        self.setupUi(self)
-
-        # Se genera la interfaz llamando al metodo setupUi
+        self.setupUi(self) # Se genera la interfaz llamando al metodo setupUi
         
+# se inicia pantalla y app
 if __name__ == '__main__':
     app = QApplication(sys.argv) # create an instance of the application
-    window = QWidget() # create an instance of a window
-    window.show() # to make the window visible
+    appWin = AppWin() # create an instance of a window
+    appWin.show() # to make the window visible
     app.exec() # to start up the event loop
