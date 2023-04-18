ed86d0968f4e8acf0b7cff66d699ef3bc8e6e9ed
MazzoniFederico <federicomazz12@gmail.com>
2023-04-17 23:02:34 -0300

    generador y muestra en pantalla

---
+++ generadores/intervalo.py
@@ -0,0 +1,10 @@
+
+
+class Intervalo:
+    def __init__(self, desde, hasta, cantidad):
+        self.desde = desde
+        self.hasta = hasta
+        self.cantidad = cantidad
+
+    def sumar():
+        cantidad += 1
