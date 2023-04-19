d0ae2a6360e973aa2259c1ed5c0f39b066400ac4
jGallese <juancruz.gallese@gmail.com>
2023-04-19 11:53:16 -0300

    Primer parte prueba chi
    Calculo de intervalos en uniforme.
    Falta calcular datos para otras variables.
    
    Hacer iteraciones de cada prueba para cada distribucion
    
    Descomentar funcion generarPruebaChi en Intervalo

--- generadores/intervalo.py
+++ generadores/intervalo.py
@@ -5,6 +5,26 @@ class Intervalo:
         self.desde = desde
         self.hasta = hasta
         self.cantidad = cantidad
+        
 
     def sumar():
         cantidad += 1
+
+    
+
+# def generarPruebaChi(tipo, datos):
+#     if(tipo == "uniforme"):
+#         #calculo con intervalos de uniforme
+#         pass
+#     elif(tipo == "normal"):
+#         #calculo con datos de normal
+#         pass
+#     elif(tipo == "exponencial"):
+#         #calculo con datos de exponencial
+        
+        
+#         pass
+#     else:
+#         #calculo con datos de poisson
+       
+#         pass
